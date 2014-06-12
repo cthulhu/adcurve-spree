b376678952e5e86b33012cba251cd961e5468144
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2014-06-12 15:32:21 +0200

    [+] conversion tag

--- app/overrides/s2m_tag.rb
+++ app/overrides/s2m_tag.rb
@@ -1,5 +1,11 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "s2m_tag",
+                     :name => "s2m_tag_landing",
                      :insert_top => "[data-hook='body']",
                      :partial => "spree/shop2market_tag/tracking_tag",
+                     :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/orders/show",
+                     :name => "s2m_tag_coversion",
+                     :insert_top => "[data-hook='body']",
+                     :partial => "spree/shop2market_tag/coversion_tag",
                      :disabled => false)
