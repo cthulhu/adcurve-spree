f49905739e68f64b93d37b1bd7d9b0717bd7f937
Stanislav O. Pogrebnyak <stanislav.pogrebnyak@gmail.com>
2016-11-24 14:10:43 +0100

    [^] Changed generator

--- lib/generators/spree_s2m_tag/install/install_generator.rb
+++ lib/generators/spree_s2m_tag/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeS2mTag
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_s2m_tag\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_s2m_tag\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require store/spree_s2m_tag\n"
+
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_s2m_tag\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_s2m_tag\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require store/spree_s2m_tag\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require admin/spree_s2m_tag\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
--- spree_s2m_tag.gemspec
+++ spree_s2m_tag.gemspec
@@ -5,7 +5,7 @@ Gem::Specification.new do |s|
   s.version     = '0.1.0'
   s.summary     = 'Shop2market tag for spree'
   s.description = 'Easy installation shop2market support for spree driven shops'
-  s.required_ruby_version = '>= 1.8.7'
+  s.required_ruby_version = '>= 2.1.6'
 
   s.author    = 'Stanislav O. Pogrebnyak'
   s.email     = 'stanislav.pogrebnyak@gmail.com'
@@ -16,8 +16,10 @@ Gem::Specification.new do |s|
   s.require_path = 'lib'
   s.requirements << 'none'
 
-  s.add_dependency 'spree_core', '~> 3.1.0'
-  s.add_dependency 'spree_frontend'
+  s.add_dependency 'spree_core', '>= 3.1.0', '< 4.0'
+  s.add_dependency 'spree_backend', '>= 3.1.0', '< 4.0'
+  s.add_dependency 'spree_frontend', '>= 3.1.0', '< 4.0'
+  s.add_dependency 'spree_api', '>= 3.1.0', '< 4.0'
 
   s.add_development_dependency 'capybara', '~> 1.1.2'
   s.add_development_dependency 'coffee-rails'
