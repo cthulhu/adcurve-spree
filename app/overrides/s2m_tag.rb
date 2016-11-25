9c2ea32eca13c204cf4f4bb57ee82aa1233d844c
rost <rostislav.korin@gmail.com>
2016-11-25 19:30:56 +0200

    sidebar

--- app/overrides/s2m_tag.rb
+++ app/overrides/s2m_tag.rb
@@ -9,3 +9,8 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :insert_bottom => "[data-hook='body']",
                      :partial => "spree/shop2market_tag/coversion_tag",
                      :disabled => false)
+
+Deface::Override.new(:name => "s2m_tag",
+                     :virtual_path => "spree/layouts/admin",
+                     :insert_after => "[data-hook='admin_configurations_sidebar_menu']",
+                     :text => "<li>s2m</li")
