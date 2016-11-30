e43ba75f5a0e1a7eb16229e39d5c55b1273be80d
rost <rostislav.korin@gmail.com>
2016-11-30 23:20:58 +0200

    rename

--- lib/generators/adcurve_spree/install/install_generator.rb
+++ lib/generators/adcurve_spree/install/install_generator.rb
@@ -2,13 +2,13 @@ module AdcurveSpree
   module Generators
     class InstallGenerator < Rails::Generators::Base
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require store/adcurve_spree\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/adcurve_spree\n"
 
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require store/adcurve_spree\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require admin/adcurve_spree\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/adcurve_spree\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/adcurve_spree\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
